bad465b6e155342356df85b4b90f52d538ee23c9
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-09-18 21:31:03 -0300

    pronuncia

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string frase;
+
+            //<---Inicio(Digite)--->//
+            Console.Write("Digite Qualquer Coisa: ");
+            frase = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{frase.Replace("r", "l").Replace("R", "L")}");
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->//
+        }
+    }
+}
